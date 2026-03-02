e264b7e5d1e5bbf0e98fd840eae3e9457dfc5949
leomatte <matte.leonardo02@gmail.com>
2026-03-02 10:16:49 -0300

    modifica a frase da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Paty, te amo very mush!</h1>
+    return <h1>Paty, te amo very mush! É isso aí bixo
+        Testando um teste bem testado.
+        Continuando a testar
+    </h1>
 }
 
 export default Home;

7c5ae6d3a1bbf05d65ed8cb80b91fca38e719e65
leomatte <matte.leonardo02@gmail.com>
2026-06-25 09:59:12 -0300

    makes `database.js` `client.connect()` more robust to errors

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,26 @@
 import database from "infra/database.js";
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "O léo é tipo assim, o cara bixo" });
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database.query("SHOW server_version");
+  const postgresMaxCon = await database.query("SHOW max_connections");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const postgresOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(postgresMaxCon.rows[0].max_connections),
+        opened_connections: postgresOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,32 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Retorna versão do Postgres
+  expect(typeof responseBody.dependencies.database.version).toBe("string");
+  expect(responseBody.dependencies.database.version).toEqual("16.13");
+  console.log(
+    "Versão do postgres: " + responseBody.dependencies.database.version,
+  );
+
+  //Retorna máximo de conexões ao banco de dados
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  console.log(
+    "Máximo de conexões: " + responseBody.dependencies.database.max_connections,
+  );
+
+  //Retorna usuários ativos:
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  console.log(
+    "Usuários ativos: " + responseBody.dependencies.database.opened_connections,
+  );
 });
